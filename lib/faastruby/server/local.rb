52836aa50d8e72009ae2de5641125a6dceee8649
Paulo Arruda <paulo@parruda.net>
2019-03-10 22:02:37 -0300

    Quit server when FaaStRuby Local fails

--- lib/faastruby/server/local.rb
+++ lib/faastruby/server/local.rb
@@ -15,7 +15,9 @@ module FaaStRuby
           system(cmd.join(" "))
           sleep 1
         end
-        puts "FaaStRuby Local exited. Please press CTRL+C."
+        puts "FaaStRuby Local exited due to an error."
+        puts "If you think this shouldn't have happened, open an issue here: https://github.com/FaaStRuby/faastruby-cli/issues/new"
+        exit 1
       end
     end
   end
