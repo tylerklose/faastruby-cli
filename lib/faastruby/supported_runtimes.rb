f4e869d877c45b9603998c7a1a53ae79cdee0812
Michael Kohl <citizen428@gmail.com>
2019-03-23 20:44:01 +0700

    Add 2.6.2 as supported Ruby version

--- lib/faastruby/supported_runtimes.rb
+++ lib/faastruby/supported_runtimes.rb
@@ -1,6 +1,6 @@
 module FaaStRuby
   # It is important that they are sorted in version order!
-  SUPPORTED_RUBY = ['2.5.3', '2.6.0', '2.6.1']
+  SUPPORTED_RUBY = ['2.5.3', '2.6.0', '2.6.1', '2.6.2']
   SUPPORTED_CRYSTAL = ['0.27.0', '0.27.2']
   CRYSTAL_LATEST = SUPPORTED_CRYSTAL.last
   RUBY_LATEST = SUPPORTED_RUBY.last
