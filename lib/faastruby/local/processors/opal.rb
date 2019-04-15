0fd70ee50825580ea2a3eba7122d3b6b1fb49cf3
Paulo Arruda <paulo@parruda.net>
2019-04-15 13:32:13 -0300

    delete empty directories recursively

--- lib/faastruby/local/processors/opal.rb
+++ lib/faastruby/local/processors/opal.rb
@@ -28,8 +28,22 @@ module FaaStRuby
 
       def remove_from_public(event)
         delete_path = event.relative_path_dirname.sub(/^#{Local.opal_dir}\//, "#{Local.public_dir}/")
-        return false unless delete_path.match(/^#{Local.public_dir}\//)
-        FileUtils.rm_f(delete_path)
+        # return false unless delete_path.match(/^#{Local.public_dir}\//)
+        return false if delete_path.nil? || delete_path.match(/^\s*$/)
+        puts "Deleting: #{Local.opal_js_destination}/#{delete_path}"
+        FileUtils.rm_rf("#{Local.opal_js_destination}/#{delete_path}")
+        dir = "#{Local.opal_js_destination}/#{delete_path}"
+        delete_recursive_folder(dir)
+      end
+
+      def delete_recursive_folder(entry)
+        dirname = File.dirname(entry)
+        return if dirname == Local.opal_js_destination
+        glob = Dir.glob("#{dirname}/*")
+        return if glob.any?
+        puts "Deleting: #{dirname}"
+        FileUtils.rmdir(dirname)
+        delete_recursive_folder(dirname)
       end
 
       def compile(event)
